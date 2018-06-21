e390545090f4cc435546a4c4ab2e2db0ce093526
zhengzheming <7upcat@gmail.com>
2018-06-21 17:43:41 +0800

    修复了非 prd profile 的 BUG，优化了 logback 的配置

--- src/main/java/org/reap/portal/Application.java
+++ src/main/java/org/reap/portal/Application.java
@@ -64,6 +64,8 @@ public class Application {
 	}
 
 	public static void main(String[] args) throws Exception {
-		new SpringApplication(Application.class).run(args);
+		SpringApplication application = new SpringApplication(Application.class);
+		application.setAdditionalProfiles("prd");
+		application.run(args);
 	}
 }
