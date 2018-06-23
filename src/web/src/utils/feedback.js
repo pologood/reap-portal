c83cfba804866acf10c7eb37081fc5ec23952ed3
zhengzheming <7upcat@gmail.com>
2018-06-23 19:54:01 +0800

    更新了 feedback 补充了 warning

--- src/web/src/utils/feedback.js
+++ src/web/src/utils/feedback.js
@@ -1,19 +1,20 @@
-import { notification as antdNotification } from 'antd';
+import { notification as antdNotification } from 'antd'
 
 antdNotification.config({
   placement: 'topRight',
   top: 32,
   duration: 3,
-});
+})
 
 export default {
   notification: {
-    error: result => {
+    error: (result) => {
       antdNotification.error({
         message: `交易失败：错误码 ${result.responseCode}`,
         description: `详细原因：${result.responseMessage}`,
-      });
+      })
     },
     success: (message, description) => antdNotification.success({ message, description }),
+    warning: (message, description) => antdNotification.warning({ message, description }),
   },
-};
+}
